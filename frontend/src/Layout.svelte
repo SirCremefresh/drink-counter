a9218c569cd9a206e703904d35be6bf955bc0b67
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2019-12-02 09:13:47 +0100

    added navbar icons

--- frontend/src/Layout.svelte
+++ frontend/src/Layout.svelte
@@ -1,5 +1,14 @@
 <script>
     export let showRoute;
+
+    function showRouteRanking() {
+        showRoute("RANKING")
+    }
+
+    function showRouteScanner() {
+        console.log("hello")
+        showRoute("SCANNER")
+    }
 </script>
 
 <div class="container">
@@ -8,7 +17,12 @@
         <slot></slot>
     </div>
     <div class="tabs">
-
+        <div class="trophy" on:click={showRouteRanking}>
+            <i class="fas fa-trophy"></i>
+        </div>
+        <div class="qrcode" on:click={showRouteScanner}>
+            <i class="fas fa-qrcode"></i>
+        </div>
     </div>
 </div>
 
@@ -16,7 +30,7 @@
     .container {
         height: 100%;
         display: grid;
-        grid-template-rows: 60px 1fr 80px;
+        grid-template-rows: 60px 1fr 50px;
         grid-template-areas: "title" "content" "tabs";
     }
 
@@ -34,8 +48,34 @@
     }
 
     .tabs {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+
+
         grid-area: tabs;
         background-color: #00d1b2;
         margin: 0;
+    }
+
+    .trophy {
+        grid-column: 1;
+        place-self: center;
+        color: #363636;
+
+        height: calc(100% - 10px);
+        width: auto;
+    }
+
+    .fa-trophy, .fa-qrcode {
+        font-size: 40px;
+    }
+
+    .qrcode {
+        grid-column: 2;
+        place-self: center;
+        color: #363636;
+
+        height: calc(100% - 10px);
+        width: auto;
     }
 </style>
